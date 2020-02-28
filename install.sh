4d0c53992b20e4d019125c7479bfdc992bf24363
Kingson Chan <kingsonchan98@gmail.com>
2020-02-28 23:52:24 +0000

    Add antibody static loading script generation to install script

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 rm -f ~/.zshrc
 rm -f ~/.gitconfig
 
+antibody bundle < ${BASEDIR}/zsh/.zsh_plugins.txt > ${BASEDIR}/zsh/.zsh_plugins.sh
+
 ln -s ${BASEDIR}/zsh/.zshrc ~/.zshrc
 ln -s ${BASEDIR}/git/.gitconfig ~/.gitconfig
 
