9789b229b209f68b8f47424ac2ca94c773ab4f82
Kingson Chan <kingsonchan98@gmail.com>
2020-02-28 21:52:57 +0000

    Add zsh-completions

--- .zsh_plugins.sh
+++ .zsh_plugins.sh
@@ -3,6 +3,8 @@ source /home/kingson/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-
 fpath+=( /home/kingson/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k )
 source /home/kingson/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
 fpath+=( /home/kingson/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /home/kingson/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /home/kingson/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
 source /home/kingson/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-gretzky-SLASH-auto-color-ls/auto-color-ls.plugin.zsh
 fpath+=( /home/kingson/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-gretzky-SLASH-auto-color-ls )
 source /home/kingson/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips/alias-tips.plugin.zsh
